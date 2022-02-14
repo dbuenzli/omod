7da4a8a95ad46e0cb6fdfb32baeb4fffe808719f
Daniel Bünzli <daniel.buenzli@erratique.ch>
2022-02-14 17:46:45 +0100

    Remove some watermarks.

--- src/omod.ml
+++ src/omod.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2018 The omod programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 (* Preliminaries *)
--- src/omod_ocamlc.ml
+++ src/omod_ocamlc.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2016 The omod programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 (* Error handling *)
